e98633283f2bbaa92e65673a2157cec86b4ddf41
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-01-26 12:43:02 -0700

    Feat: Fix typo

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,7 +5,7 @@ const yaml = require("js-yaml");
 
 exports.write2Yaml = (data) => {
   let contents = {
-    name: `carborn-${data}`, // change here for unique name
+    name: `carbon-${data}`, // change here for unique name
     memory: "64M",
     buildpack: "https://github.com/cloudfoundry/staticfile-buildpack.git",
   };
